584a46154a164a39fc7beb12c5a318d23111048d
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-06-21 00:57:36 +0500

    Created model class

--- Url_Shortener/Data/AppDbContext.cs
+++ Url_Shortener/Data/AppDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Url_Shortener.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Url_Shortener.Models;
+
+namespace Url_Shortener.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ShortUrl> ShortUrls => Set<ShortUrl>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortUrl>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.OriginalUrl).IsRequired().HasMaxLength(2048);
+                entity.Property(e => e.ShortCode).HasMaxLength(20);
+                entity.HasIndex(e => e.ShortCode).IsUnique();
+            });
+        }
     }
 }
--- Url_Shortener/Program.cs
+++ Url_Shortener/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Url_Shortener.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Database connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(connectionString, serverVersion);
+});
+
+
+
+
+
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
